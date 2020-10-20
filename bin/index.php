86760341a54d32eb9babf5e3ea7a34992e855e7f
柳州市赖可龙 <2227395155@qq.com>
2020-10-20 16:13:01 +0800

    update

--- bin/index.php
+++ bin/index.php
@@ -9,8 +9,10 @@ date_default_timezone_set('Asia/Shanghai');
 ! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
 
 require BASE_PATH . '/vendor/autoload.php';
+
 $config = new \src\config\Config(require BASE_PATH . '/config/autoload/commands.php');
-$file = $config->get('command');
+$command = $config->get('command');
+
 $application = new \Symfony\Component\Console\Application();
-$application->add(new $file);
+$application->add(new $command);
 $application->run();
