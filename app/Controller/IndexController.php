bb07dcb4ec5c58d58e7c2ad50859b19a5c6618a8
柳州市赖可龙 <2227395155@qq.com>
2020-10-26 17:47:00 +0800

    study...

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -6,9 +6,25 @@ namespace App\Controller;
 
 class IndexController
 {
+    const PRICE = 0.02;
+
+    const BASE_PRICE = 200;
+
     public function index($id)
     {
-        var_dump($id);
-        return 1;
+        $points = 10000;
+        $money  = 200;
+        $points1 = $points - ($money / self::PRICE);
+        $discount = round(($points1 / $points)  * 10, 1);
+
+        if ($discount) {
+            $proportion = round(10 - $discount, 1);
+            var_dump($proportion);
+            var_dump('OR');
+            var_dump('2-(points / (money / BASE_PRICE * (BASE_PRICE / PRICE)))');
+        } else {
+            var_dump('No discount');
+        }
+        return true;
     }
 }
