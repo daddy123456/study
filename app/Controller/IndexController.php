0627f1ccfe5162e912a5fd838b737702f19b07c7
柳州市赖可龙 <2227395155@qq.com>
2020-11-02 12:11:46 +0800

    test interface

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -7,25 +7,15 @@ namespace App\Controller;
 
 class IndexController
 {
-    const PRICE = 0.02;
-
-    const BASE_PRICE = 200;
-
     public static function index()
     {
-        $points = 10000;
-        $money  = 200;
-        $points1 = $points - ($money / self::PRICE);
-        $discount = round(($points1 / $points)  * 10, 1);
-
-        if ($discount) {
-            $proportion = round(10 - $discount, 1);
-            var_dump($proportion);
-            var_dump('OR');
-            var_dump('2-(points / (money / BASE_PRICE * (BASE_PRICE / PRICE)))');
-        } else {
-            var_dump('No discount');
-        }
+        $url = 'http://api.btstu.cn/sjbz/api.php?format=json';
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        var_dump($response);
         return true;
     }
 
